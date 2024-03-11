7c4e75216b3593663a35ccec85d598c3dfdc5422
Ozansk <ozansk51@gmail.com>
2024-03-11 20:13:48 +0300

    class validation added

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 import baseConfig from 'baseConfig';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+    app.useGlobalPipes(new ValidationPipe());
     await app.listen(baseConfig.SERVER_PORT);
 }
 bootstrap();
--- src/users/dto/createUser.dto.ts
+++ src/users/dto/createUser.dto.ts
@@ -1,8 +1,25 @@
+import { IsNotEmpty, IsString, Length } from 'class-validator';
+
 class CreateUserDto {
+    @IsNotEmpty()
+    @IsString()
     firstName: string;
+
+    @IsNotEmpty()
+    @IsString()
     lastName: string;
+
+    @IsNotEmpty()
+    @IsString()
     userName: string;
+
+    @IsNotEmpty()
+    @IsString()
     password: string;
+
+    @IsNotEmpty()
+    @IsString()
+    @Length(10, 10)
     phoneNumber: string;
 }
 
